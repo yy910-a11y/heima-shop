497a0ad751e04bd495b619705721a5c2fbc498db
[200~yy910-a11y~ <3075389167@qq.com>
2025-04-19 22:07:56 +0800

    商品详情-页面渲染

--- src/services/goods.ts
+++ src/services/goods.ts
@@ -1,9 +1,9 @@
 ///goods
 //商品详情
 import { http } from '@/utils/http'
-
+import type { GoodsResult } from '@/types/goods'
 export const getGoodsByIdAPI = (id: string) => {
-  return http({
+  return http<GoodsResult>({
     method: 'GET',
     url: '/goods',
     data: {
